e8af7d22c0b66243592d022e1cb39e388f49af01
Sohaib <sohaibm131@gmail.com>
2024-09-12 21:28:23 -0400

    Updated demo.java

--- JavaProgramming/src/WeekTwo/demo.java
+++ JavaProgramming/src/WeekTwo/demo.java
@@ -2,6 +2,6 @@ package WeekTwo;
 
 public class demo {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Java Programmers");
     }
 }
