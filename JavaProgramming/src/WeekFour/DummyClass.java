4d2beffad40b0cfe6ddfd310d411750caa1b0d4f
Sohaib <sohaibm131@gmail.com>
2024-10-01 20:27:00 -0400

    Updated

---
+++ JavaProgramming/src/WeekFour/DummyClass.java
@@ -0,0 +1,12 @@
+package WeekFour;
+
+// java, jwt
+import WeekTwo.Person;
+public class DummyClass {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("Sohaib");
+
+        System.out.println(person.getName());
+    }
+}
---
+++ JavaProgramming/src/WeekFour/LessonFourExamples/Day.java
@@ -0,0 +1,5 @@
+package WeekFour.LessonFourExamples;
+
+public enum Day {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
